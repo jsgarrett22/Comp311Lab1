efa990635127e10e44662dc355e61e0cc94c7648
Joshua Garrett <garrej01@email.franklin.edu>
2025-01-31 13:29:44 -0500

    Implemented SymbolTable collection that holds Symbols and their appropriate values

--- src/com/garrett/SymbolTable.java
+++ src/com/garrett/SymbolTable.java
@@ -1,18 +1,24 @@
 package com.garrett;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Manages the available variables.
+ * Manages the available variables via a list of Symbols.
  * 
- * @author Tim
- * @version Jan 13, 2008
+ * @author Tim and Joshua S. Garrett
+ * @version Jan 13, 2008 - Revised on 01/31/2025 by Joshua S. Garrett
  */
 public class SymbolTable
 {
+	private List<Symbol> symbols;
+	
     /**
      * Creates a SymbolTable.
      */
     public SymbolTable()
     {
+    	symbols = new ArrayList<Symbol>();
     }
     
     /**
@@ -20,20 +26,55 @@ public class SymbolTable
      * 
      * @param name the name
      * @param value the value
+     * @author Joshua S. Garrett
      */
     public void setValue(String name, int value)
     {
+    	if (!hasSymbol(name)) {
+    		symbols.add(new Symbol(name, value));
+    	}
     }
     
     /**
      * Returns the value of the given variable.
-     * 
      * @param name the name
      * @return the value
      * @throws RuntimeException if the variable is not defined
+     * @author Joshua S. Garrett
      */
     public int getValue(String name)
     {
-        return 0;
+    	for (int i = 0; i < symbols.size(); i++) {
+    		Symbol currentSymbol = symbols.get(i);
+    		if (currentSymbol.getSymbol().equals(name)) {
+    			return currentSymbol.getValue();
+    		}
+    	}
+        throw new RuntimeException("The variable is not defined in the table.");
+    }
+    
+    /**
+     * Returns the size of the symbol table.
+     * @return the number of symbols currently in the table.
+     * @author Joshua S. Garrett
+     */
+    public int size() {
+    	return symbols.size();
+    }
+    
+    /**
+     * Determines if the symbol table currently contains the symbol or not.
+     * @param string The symbol to check.
+     * @return boolean true if the symbol table current contains that symbol, false if not.
+     * @author Joshua S. Garrett
+     */
+    public boolean hasSymbol(String symbol) {
+    	for (int i = 0; i < symbols.size(); i++) {
+    		Symbol currentSymbol = symbols.get(i);
+    		if (currentSymbol.getSymbol().equals(symbol)) {
+    			return true;
+    		}
+    	}
+    	return false;
     }
 }
---
+++ src/com/garrett/test/SymbolTableTest.java
@@ -0,0 +1,67 @@
+package com.garrett.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.security.InvalidParameterException;
+
+import org.junit.Test;
+
+import com.garrett.SymbolTable;
+
+/**
+ * Tests the SymbolTable class.
+ * @author Joshua S. Garrett
+ * @version Jan 31st, 2025
+ */
+public class SymbolTableTest {
+
+	@Test
+	public void testSymbolTableDefaultConstructor() {
+		assertNotNull(new SymbolTable());
+	}
+
+	@Test
+	public void testGetAndSetValue() {
+		SymbolTable symbols = new SymbolTable();
+		
+		/* Set an invalid symbol in the table and catch the exception */
+		assertThrows("A symbol can not be empty",
+				InvalidParameterException.class, 
+				() -> symbols.setValue("", 100));
+		
+		/* Set a valid symbol in the table and retrieve it */
+		symbols.setValue("x", 5);
+		assertEquals(5, symbols.getValue("x"));
+		
+		/* Try setting a duplicate symbol in the table and retrieve it */
+		symbols.setValue("x", 5);
+		assertEquals(5, symbols.getValue("x"));
+		assertEquals(1, symbols.size());
+		
+		/* Try getting an undefined symbol */
+		assertThrows("A symbol can not be empty",
+				RuntimeException.class, 
+				() -> symbols.getValue("a"));
+	}
+	
+	/**
+	 * Tests whether the symbol table contains a particular symbol or not.
+	 */
+	@Test
+	public void testhasSymbol() {
+		SymbolTable symbols = new SymbolTable();
+		symbols.setValue("x", 5);
+		symbols.setValue("y", 10);
+		symbols.setValue("z", 10);
+		assertEquals(3, symbols.size());
+		assertTrue(symbols.hasSymbol("x"));
+		assertTrue(symbols.hasSymbol("y"));
+		assertTrue(symbols.hasSymbol("z"));
+		assertFalse(symbols.hasSymbol("a"));
+	}
+
+}
